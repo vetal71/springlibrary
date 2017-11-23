4609c7076cb2712ae0a60df37a76a6077b251e17
vetal <vetalkolesnik@gmail.com>
2017-11-23 13:24:25 +0300

    create project

---
+++ src/main/java/by/softclub/training/library/springlibrary/SpringlibraryApplication.java
@@ -0,0 +1,12 @@
+package by.softclub.training.library.springlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlibraryApplication.class, args);
+	}
+}
